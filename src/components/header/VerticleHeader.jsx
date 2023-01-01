01293f3d27f06c7254db6878d000724a0457130f
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-01-01 22:35:32 +0530

    update: hover efft in side nav

--- src/components/header/VerticleHeader.jsx
+++ src/components/header/VerticleHeader.jsx
@@ -11,26 +11,26 @@ import { IconContext } from "react-icons";
 
 function VerticleHeader() {
   return (
-    <header className="flex flex-col h-screen w-12 bg-slate-900 border-slate-700 justify-center">
+    <header className="flex flex-col h-screen px-4 bg-slate-900 border-slate-700 justify-center">
       <nav>
         <ul className="flex flex-col items-center justify-center my-16  gap-8">
           <li>
             <Link to="/">
-              <div className="bg-slate-700 rounded-full p-2">
+              <div className="bg-slate-700 rounded-full p-2 hover:bg-slate-400">
                 <FaHome className="w-6 h-6 text-white hover:text-slate-100" />
               </div>
             </Link>
           </li>
           <li>
             <Link to="/services">
-              <div className="bg-slate-700 rounded-full p-2">
+              <div className="bg-slate-700 rounded-full p-2 hover:bg-slate-400">
                 <MdOutlineMiscellaneousServices className="w-6 h-6 text-white hover:text-slate-100" />
               </div>
             </Link>
           </li>
           <li>
             <Link to="/projects">
-              <div className="bg-slate-700 rounded-full p-2">
+              <div className="bg-slate-700 rounded-full p-2 hover:bg-slate-400">
                 <IconContext.Provider
                   value={{
                     color: "white",
@@ -43,14 +43,14 @@ function VerticleHeader() {
           </li>
           <li>
             <Link to="/workhistory">
-              <div className="bg-slate-700 rounded-full p-2">
+              <div className="bg-slate-700 rounded-full p-2 hover:bg-slate-400">
                 <MdHomeWork className="w-6 h-6 text-white hover:text-slate-100" />
               </div>
             </Link>
           </li>
           <li>
             <Link to="/contact">
-              <div className="bg-slate-700 rounded-full p-2">
+              <div className="bg-slate-700 rounded-full p-2 hover:bg-slate-400">
                 <MdPermContactCalendar className="w-6 h-6 text-white hover:text-slate-100" />
               </div>
             </Link>
