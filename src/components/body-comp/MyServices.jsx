d5b947f9b0cde23cad8d7edf2e2302df46e9ec00
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-01-12 22:29:02 +0530

    update: my services

--- src/components/body-comp/MyServices.jsx
+++ src/components/body-comp/MyServices.jsx
@@ -9,31 +9,31 @@ function MyServices() {
     {
       title: "Web Development",
       description:
-        "I am experienced in Full Stack web development, which include the use of Javascript, HTML, CSS, and back-end technologies like NodeJs and Python. I offer website development services for small businesses, Blogs, E-commerce, and startup projects.",
+        "FrontEnd Development Using React and NextJs and BackEnd Development Using NodeJs and Python.",
       icon: <GrPersonalComputer />,
     },
     {
       title: "Mobile App Development",
       description:
-        "I am also experienced in Mobile App development using React Native and Flutter. I have developed many mobile apps for small businesses and startups.",
+        "Mobile app Dev Using Dart and Flutter",
       icon: <BiMobileAlt />,
     },
     {
       title: "3D Animation",
       description:
-        "I am also experienced in 3D Animation and 3D Modeling. I have worked on many 3D projects for small businesses and startups.",
+        "3D animation design using Blender and Iclone.",
       icon: <GiCubes />,
     },
     {
       title: "Digital potrait painting",
       description:
-        "I create Digital potrait patings using Adobe illustraitor and Photoshop. I have worked on many 3D projects for small businesses and startups.",
+        "Adobe Illustrator and Adobe Photoshop",
       icon: <BiPaint />,
     },
     {
       title: "Game Development",
       description:
-        "I develop games using Unity3D and Unreal Engine. I have worked on many 3D projects for small businesses and startups.",
+        "Game Development Using Unity and C#, and also using Unreal Engine and C++",
       icon: <GrGamepad />,
     },
   ];
@@ -44,15 +44,14 @@ function MyServices() {
           My Services
         </h1>
         <div className="flex flex-row justify-center text-slate-300 text-md font-bold my-2">
-          "I am experienced in Full Stack web development, which include the use
-          of Javascript, HTML, CSS, and back-end technologies like NodeJs and
+          I am experienced in Full Stack web development, which include the use
+          of Javascript, HTML, CSS, and also modern solutions like React and NextJs and back-end technologies like NodeJs and
           Python. I offer website development services for small businesses,
           Blogs, E-commerce, and startup projects.
         </div>
         <div className="flex flex-row justify-center text-slate-300 text-md font-bold my-2">
-          I am also experienced in Mobile App development using React Native and
-          Flutter. I have developed many mobile apps for small businesses and
-          startups.
+          I am also experienced in Mobile App development using 
+          Flutter. I have developed many mobile apps for small businesses and as solo Projects.
         </div>
         <div className="flex flex-row justify-center text-slate-300 text-md font-bold my-2">
           I am also experienced in 3D Animation and 3D Modeling. I have worked
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,8 +5,8 @@ import MyServices from "../body-comp/MyServices";
 
 function HomePage() {
   return (
-    <div className="h-full left-64 w-[calc(100vw-22rem)] relative bg-gradient-to-r from-slate-400 to-slate-900">
-      <div className="absolute  bg-gradient-to-r from-slate-400 to-slate-900">
+    <div className="h-full left-64 w-[calc(100vw-22rem)] relative">
+      <div className="absolute  bg-gradient-to-r from-slate-500 to-slate-900">
         <HeroSection />
         <MyServices />
         <BodyComponent />
