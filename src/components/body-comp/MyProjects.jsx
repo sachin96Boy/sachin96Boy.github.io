ce221f484b627cc3d356d11a41a510cc884c8aef
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-02-27 21:50:02 +0530

    chor: add fiverr on to portfolio

--- src/components/body-comp/MyProjects.jsx
+++ src/components/body-comp/MyProjects.jsx
@@ -6,15 +6,22 @@ import axios from "axios";
 
 function MyProjects() {
   const [projects, setProjects] = useState();
+  const [social, setSocial] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
-      .get(`${import.meta.env.VITE_DATABASE_URL}Projects.json`)
-      .then((res) => {
-        setProjects(res.data);
-        setLoading(false);
-      })
+      .all([
+        axios.get(`${import.meta.env.VITE_DATABASE_URL}Projects.json`),
+        axios.get(`${import.meta.env.VITE_DATABASE_URL}social.json`),
+      ])
+      .then(
+        axios.spread((res1, res2) => {
+          setProjects(res1.data);
+          setSocial(res2.data);
+          setLoading(false);
+        })
+      )
       .catch((err) => {
         console.log(err);
       });
@@ -29,11 +36,17 @@ function MyProjects() {
   }
 
   return (
-    <div className="flex flex-col w-full items-center">
+    <div className="flex flex-col w-full items-center min-h-screen">
       <div className="pt-4">
         <h1 className="text-3xl font-black text-slate-300">
           My Portfolio Projects
         </h1>
+      </div>
+      <div className="text-2xl text-slate-300 text-center">
+        Check out my 3D Animation Works on{" "}
+        <a href={social?.fiverr}>
+          <span className="text-green-500 font-black text-3xl">Fiverr</span>
+        </a>
       </div>
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2 items-center mx-4">
         {projectExperience}
