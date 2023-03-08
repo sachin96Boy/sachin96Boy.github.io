c0c7530892b6211460b9dff8dd952eefe60161e4
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-03-08 14:31:47 +0530

    chor: select prouction

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
     <BrowserRouter>
       <App />
-      <Analytics />
+      <Analytics mode="production"/>
     </BrowserRouter>
   </ThemeProvider>
 );
