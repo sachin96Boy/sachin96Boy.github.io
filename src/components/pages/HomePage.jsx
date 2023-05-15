80603b737663e0ef74b551a7628e2215430d02fd
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-05-15 08:54:08 +0530

    update: footer hyperlinks

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import ContactMe from "../body-comp/ContactMe";
 
 function HomePage() {
   return (
-    <div className="h-full w-full lg:left-64 lg:w-[calc(100vw-18rem)] relative">
+    <div className="w-full lg:left-64 lg:w-[calc(100vw-18rem)] relative">
       <div className="absolute m-0 right-0 lg:right-12 left-0  bg-gradient-to-r from-slate-400 to-slate-900">
         <HeroSection />
         <MyServices />
