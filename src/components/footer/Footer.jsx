e525d1827699b3a0058425418711ee1cee63a5eb
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-10-03 07:01:46 +0530

    chor: uris

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -28,7 +28,7 @@ function Footer() {
             </a>
 
             <a
-              href="https://www.twitter.com/SACHINSUPUNTHAK"
+              href="https://www.twitter.com/sachin96boy"
               type="button"
               className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1"
             >
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -222,10 +222,10 @@ function Sidebar(props) {
             <a dir="https://www.facebook.com/Sachin96boy">
               <BsFacebook className="w-6 h-6 mr-3 text-slate-400 hover:text-slate-100" />
             </a>
-            <a href="https://www.linkedin.com/in/sachin-supunthaka-9b5b3b1b3/">
+            <a href="https://www.linkedin.com/in/sachin96boy/">
               <BsLinkedin className="w-6 h-6 mr-3 text-slate-400 hover:text-slate-100" />
             </a>
-            <a href="https://www.twitter.com/SACHINSUPUNTHAK">
+            <a href="https://www.twitter.com/sachin96boy">
               <BsTwitter className="w-6 h-6 mr-3 text-slate-400 hover:text-slate-100" />
             </a>
             <a href="https://www.github.com/Sachin96Boy">
